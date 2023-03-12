7134e5f2633885d850f1a0be8d83c97995fabe45
kingwangjjang30 <109188473+kingwangjjang30@users.noreply.github.com>
2023-03-12 15:53:06 +0900

    Add files via upload
    java scanning and printing review(very short)

---
+++ java_st.java
@@ -0,0 +1,16 @@
+package java12;
+
+import java.util.Scanner;
+
+public class java_st {
+
+	public static void main(String[] args) {
+		String major, num,name;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("학과 : ");
+		major = sc.next();
+		System.out.println("저는"+major+"과 학생입니다.");
+
+	}
+
+}
